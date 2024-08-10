0d1f3cd0e81e2dd6aa2c2818db51e17ab721893c
Shriya0903 <shriyasamaddar932@gmail.com>
2024-08-10 17:04:06 +0530

    (admins)

--- Frontend/src/Components/Admins/Admins.jsx
+++ Frontend/src/Components/Admins/Admins.jsx
@@ -7,7 +7,7 @@ const Admins = () => {
     <div className="min-h-1/2 py-4 pb-10 w-full flex justify-center bg-gray-100 ">
       <div className="flex flex-col w-full items-center">
         <div className="flex justify-center w-full">
-          <h1 className="text-2xl">MEMBERS:</h1>
+          <h1 className="text-3xl ">MEMBERS</h1>
         </div>
         <div className="w-1/2 pt-12 items-center flex justify-center gap-10">
           <div className="max-w-sm mx-auto h-[60vh]  bg-white shadow-lg rounded-lg overflow-hidden">
@@ -42,7 +42,7 @@ const Admins = () => {
                     </svg>
                   </a>
                   <a
-                    href="tel:+1234567890"
+                    href="tel:+91 7218209234"
                     class="inline-flex items-center px-4 py-2   text-white font-medium rounded-md hover:bg-gray-100 transition duration-300"
                   >
                     <IoCall color="green" className="scale-150" />
@@ -66,7 +66,7 @@ const Admins = () => {
             <div className="bg-cover bg-center h-56 p-4 flex flex-col justify-center">
               <div className="h-full flex items-center justify-center ">
                 <img
-                  src={Prajwal}
+                  src={Shriya}
                   alt="Employee"
                   className="rounded-full border-4 border-white h-40 w-40"
                 />
@@ -74,13 +74,13 @@ const Admins = () => {
             </div>
             <div className="p-4 ">
               <h2 className="text-2xl font-semibold text-gray-800">
-                Prajwal Wandhare
+                Shriya Samaddar
               </h2>
               <p className="text-gray-600">Full-Stack Developer</p>
               <div className="mt-4 ">
                 <div className="flex items-center mt-2 gap-3 text-gray-600 w-full">
                   <a
-                    href="https://www.linkedin.com/in/prajwal-wandhare-four5/"
+                    href="https://www.linkedin.com/in/shriya-samaddar-bb0898200//"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
@@ -94,7 +94,7 @@ const Admins = () => {
                     </svg>
                   </a>
                   <a
-                    href="tel:+1234567890"
+                    href="tel:+91 9588633019"
                     class="inline-flex items-center px-4 py-2   text-white font-medium rounded-md hover:bg-gray-100 transition duration-300"
                   >
                     <IoCall color="green" className="scale-150" />
